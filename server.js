22b1a74e1bcade67b07d303d3fdc81051a791886
Marco <marc.trentin06@gmail.com>
2022-01-12 22:00:45 +0100

    config + dbconnection

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
-const PORT = 3000
+
+const config = require('./config/config').getConfig()
+require('./config/db')
+const PORT = config.PORT
+
+console.log( '✔ Bootstrapping Application' );
+console.log( `✔ Mode: ${config.MODE}` );
+console.log( `✔ Port: ${PORT}` );
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}! open http://localhost:${PORT}/`))
+app.listen(PORT, () => console.log(`App listening on port ${PORT}! \nDirect url : http://localhost:${PORT}/`))
